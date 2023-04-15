b77a6fca32f48db6309abcc0c463fa2c9777556e
kpetrosky <kpetrosky36@gmail.com>
2023-04-15 12:48:42 -0700

    added tables within schema.sql with each section required- department, role, employee

--- schema.sql
+++ schema.sql
@@ -1,12 +1,28 @@
-DROP DATABASE IF EXISTS books_db;
--- Creates the "books_db" database --
-CREATE DATABASE books_db;
+DROP DATABASE IF EXISTS mybusiness_db;
+-- Creates the "mybusiness_db" database --
+CREATE DATABASE mybusiness_db;
 
--- Use the books_db --
-USE books_db;
+-- Use the mybusiness_db --
+USE mybusiness_db;
 
--- Creates the table "biographies" within books_db --
-CREATE TABLE biographies (
+-- Creates the table "department" within mybusiness_db --
+CREATE TABLE department (
+  -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
+  id INT,
+  -- Makes a string column called "name" which cannot contain null --
+  name VARCHAR(100)
+);
+
+-- Creates the table "role" within mybusiness_db --
+CREATE TABLE role (
+  -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
+  id INT,
+  -- Makes a string column called "name" which cannot contain null --
+  name VARCHAR(100)
+);
+
+-- Creates the table "role" within mybusiness_db --
+CREATE TABLE role (
   -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
   id INT,
   -- Makes a string column called "name" which cannot contain null --
